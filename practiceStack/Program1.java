3e2c12a4cfd8e1b2aca5c42d2e78dc58225deb7e
Anna Valdez <112522961+Azilia026@users.noreply.github.com>
2024-02-17 15:23:51 -0600

    Add files via upload

---
+++ practiceStack/Program1.java
@@ -0,0 +1,11 @@
+public class Program1 {
+    public static void main(String[] args){
+        charStack stack = new charStack();
+        stack.init();
+        stack.stringToStack("bob");
+        stack.stringToStack("aet nomel");
+        stack.stringToStack("snoino dna revil etah I");
+        stack.stringToStack("SELUR LOBOC");
+
+    }    
+}
